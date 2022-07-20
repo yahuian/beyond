9e97b74e70538952f8eddb0083596ea78ca9c110
yahuian <yahuian@126.com>
2022-07-20 23:14:28 +0800

    basic ui layout

---
+++ ui/src/App.jsx
@@ -0,0 +1,63 @@
+import { React, useState } from 'react';
+import { BrowserRouter, NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import { StarTwoTone, MoneyCollectTwoTone, } from '@ant-design/icons';
+import './App.css';
+import News from './pages/News';
+import Bill from './pages/Bill';
+
+const { Content, Footer, Sider } = Layout;
+
+const routers = [
+  {
+    key: '/news',
+    icon: <StarTwoTone />,
+    label: (
+      <NavLink to="/news">订阅信息</NavLink>
+    ),
+    component: <News />
+  },
+  {
+    key: '/bill',
+    icon: <MoneyCollectTwoTone />,
+    label: (
+      <NavLink to="/bill">每日记账</NavLink>
+    ),
+    component: <Bill />
+  },
+]
+
+export default function App() {
+  const [collapsed, setCollapsed] = useState(true);
+  return (
+    <BrowserRouter>
+      <Layout style={{ minHeight: '100vh', }}>
+        <Sider
+          collapsedWidth='60'
+          collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}
+          style={{ background: 'white' }}
+        >
+          <div className="logo" />
+          <Menu theme="light" defaultSelectedKeys={[routers[0].key]} mode="inline" items={routers}></Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content style={{ margin: '16px 16px', }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360, }}>
+              <Routes>
+                {routers.map((p) => { return (<Route key={p.key} path={p.key} element={p.component}></Route>) })}
+                <Route path='*' element={<Navigate to={routers[0].key} />} />
+              </Routes>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Powered by <a href='https://github.com/YahuiAn/beyond'>YahuiAn/beyond</a>
+          </Footer>
+        </Layout>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+// TODO 适配移动端
+// TODO 侧边栏固定不动
+// BUG 直接在地址栏输入路由，左侧导航栏未高亮显示
---
+++ ui/src/pages/Bill/index.jsx
@@ -0,0 +1,160 @@
+import { Space, Table, Tag } from 'antd';
+import { AndroidOutlined, AppleOutlined } from '@ant-design/icons';
+import { Tabs } from 'antd';
+import React from 'react';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a href='www.baidu.com'>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a href='www.baidu.com'>Invite {record.name}</a>
+        <a href='www.baidu.com'>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '4',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '5',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '6',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '7',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '8',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '9',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '10',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '11',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+  {
+    key: '12',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+const { TabPane } = Tabs;
+
+export default function Bill() {
+  return (
+    <div>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab={<span> <AppleOutlined />Tab 1 </span>} key="1"></TabPane>
+        <TabPane tab={<span><AndroidOutlined />Tab 2</span>} key="2"></TabPane>
+      </Tabs>
+
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={{ size: 'small', total: (data.length), defaultPageSize: 20 }} />
+    </div>
+  )
+}
